96fb40f73b611c7fa4ccf77d7f098cf20d3dc4e0
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2023-10-08 15:36:20 +0530

    Weather-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+import Weather24 from './Component/Weather24/Weather24.jsx';
+
+
+function App() {
+  return (
+    <>
+   
+    <Weather24/>
+    </>
+
+
+  
+  )
+}
+
+export default App;
